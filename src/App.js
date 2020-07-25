879613b02adff951b1d4461e8ddbf763c5789fb1
Muhibullah-09 <mc2840696@gmail.com>
2020-07-25 06:13:26 +0500

    Add images

--- src/App.js
+++ src/App.js
@@ -5,33 +5,36 @@ import Chart from './components/Chart/Chart'
 import CountryPicker from './components/CountryPicker/CountryPicker'
 import styles from './App.module.css'
 import { fetchData } from './api/index'
-import coronaLogo from './images/image.png';
+import coronaLogo from './images/covid.gif';
+import Footer from './components/Footer'
 class App extends React.Component {
     //Data Ko Cards component ma use krny k liye usko as prop pass krrha hn
-    state ={
-        data : {},
+    state = {
+        data: {},
         country: '',
     }
-    async componentDidMount(){
+    async componentDidMount() {
         const fetchedData = await fetchData();
-        this.setState({ data : fetchedData });
+        this.setState({ data: fetchedData });
     }
 
-    handleCountryChange = async (country) =>{
+    handleCountryChange = async (country) => {
         const fetchedData = await fetchData(country);
-        this.setState({ data: fetchedData , country: country});
+        this.setState({ data: fetchedData, country: country });
     }
 
-    render(){
-        const { data , country } = this.state;
-        return(
-            <div className={styles.container}>
-                <h3 className={styles.logo}><a href='https://github.com/Muhibullah-09/Corona-Tracker-App-' rel="noopener noreferrer" target = '_blank'>Muhibullah Khan Kamali</a></h3>
-                <img className={styles.image} src={coronaLogo} alt='COVID-19'/>
-                <CountryPicker handleCountryChange={this.handleCountryChange} />
-                <Cards data={data} />
-                <Chart data={data} country={country} />
-            </div>
+    render() {
+        const { data, country } = this.state;
+        return (
+            <>
+                <div className={styles.container}>
+                    <img className={styles.image} src={coronaLogo} alt='COVID-19' />
+                    <CountryPicker handleCountryChange={this.handleCountryChange} />
+                    <Cards data={data} />
+                    <Chart data={data} country={country} />
+                </div>
+                <Footer />
+            </>
         );
     }
 }
